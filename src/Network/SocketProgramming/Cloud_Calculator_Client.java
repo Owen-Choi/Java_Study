1db20ed852507961d63fea6ebcdd45b94a71e860
Owen-Choi <oldstyle482@gmail.com>
2021-09-29 23:00:01 +0900

    네트워크 수업 : 클라우드 계산기(일부 완료)

--- src/Network/SocketProgramming/Cloud_Calculator_Client.java
+++ src/Network/SocketProgramming/Cloud_Calculator_Client.java
@@ -7,23 +7,24 @@ public class Cloud_Calculator_Client {
     public static void main(String[] args) throws Exception{
         String IPAddress = "127.0.0.1";
         int Pnum = 10002;
+        // 이제 이 변수 2개를 파일에서 읽어오는 작업 필요함 ::
         Socket client = new Socket(IPAddress, Pnum);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         DataInputStream FromServer = new DataInputStream(client.getInputStream());
         DataOutputStream ToServer = new DataOutputStream(client.getOutputStream());
-        /*String temp = br.readLine();
-        System.out.println(temp);*/
         System.out.println(":: Welcome to Cloud Calculator! ::");
         System.out.println("Please Enter Command with the format : ");
         System.out.println("\t (Arithmetic Expression) Integer1 Integer2");
-        System.out.println("\t Possible Arithmetic Expression : ADD, MINUS, DIV, MOD \n");
+        System.out.println("\t Possible Arithmetic Expression : ADD, MINUS, DIV, MULTIPLE \n");
+        System.out.println("\t If you want to terminate calculator, please enter 'Exit' ");
         while(true) {
-            try {
-                ToServer.writeUTF(br.readLine());
-                System.out.println(FromServer.readUTF());
-            } catch (IOException e) {
-                System.out.println("Error");
-            }
+            System.out.print("Please Enter the Command : ");
+                try {
+                    ToServer.writeUTF(br.readLine());
+                    System.out.println(FromServer.readUTF());
+                } catch (IOException e) {
+                    System.out.println("Error from Client");
+                }
         }
     }
 }
--- src/Network/SocketProgramming/Cloud_Calculator_Server.java
+++ src/Network/SocketProgramming/Cloud_Calculator_Server.java
@@ -20,7 +20,8 @@ public class Cloud_Calculator_Server {
                 String temp = FromClient.readUTF();
                 InputProcessor(temp, ToClient);
             } catch (IOException e) {
-                System.out.println("Error");
+                System.out.println("There's no more client here. terminating program");
+                System.exit(0);
             }
         }
     }
@@ -28,44 +29,57 @@ public class Cloud_Calculator_Server {
         st = new StringTokenizer(Input, " ");
         // If the token number is less than 3, then the command Parameter is out of format ::
         if(st.countTokens() != 3) {
-            ToClient.writeBytes(st.countTokens() > 3 ? "Too many arguments" : "argument is not enough");
+            ToClient.writeUTF(st.countTokens() > 3 ? "Incorrect : Too many arguments" : "Incorrect : argument is not enough");
             return;
         }
-        int First, Second;
-        switch (st.nextToken()) {
+        long First, Second;
+        long result;
+        switch (st.nextToken().toUpperCase()) {
             case "ADD" -> {
-                First = Integer.parseInt(st.nextToken());
-                Second = Integer.parseInt(st.nextToken());
-                if (Math.abs(First + Second) < Integer.MAX_VALUE)
-                    ToClient.writeUTF(Integer.toString(First + Second));
-                else
-                    ToClient.writeUTF("Arithmetic result is out of integer's range");
+                try {
+                    First = Long.parseLong(st.nextToken());
+                    Second = Long.parseLong(st.nextToken());
+                    result = First + Second;
+                    ToClient.writeUTF(Long.toString(result));
+                }catch(Exception e) {
+                    ToClient.writeUTF(e.toString());
+                }
             }
             case "MINUS" -> {
-                First = Integer.parseInt(st.nextToken());
-                Second = Integer.parseInt(st.nextToken());
-                if (Math.abs(First - Second) < Integer.MAX_VALUE)
-                    ToClient.writeUTF(Integer.toString(First - Second));
-                else
-                    ToClient.writeUTF("Arithmetic result is out of integer's range");
+                try {
+                    First = Long.parseLong(st.nextToken());
+                    Second = Long.parseLong(st.nextToken());
+                    result = First - Second;
+                    ToClient.writeUTF(Long.toString(result));
+                }catch (Exception e) {
+                    ToClient.writeUTF(e.toString());
+                }
             }
             case "MULTIPLE" -> {
-                First = Integer.parseInt(st.nextToken());
-                Second = Integer.parseInt(st.nextToken());
-                if (Math.abs(First * Second) < Integer.MAX_VALUE)
-                    ToClient.writeUTF(Integer.toString(First * Second));
-                else
-                    ToClient.writeUTF("Arithmetic result is out of integer's range");
+                try {
+                    First = Long.parseLong(st.nextToken());
+                    Second = Long.parseLong(st.nextToken());
+                    result = (long)First * Second;
+                    ToClient.writeUTF(Long.toString(result));
+                } catch (Exception e) {
+                    ToClient.writeUTF(e.toString());
+                    return;
+                }
             }
             case "DIV" -> {
-                First = Integer.parseInt(st.nextToken());
-                Second = Integer.parseInt(st.nextToken());
-                if (/*First == 0 || */Second == 0)
-                    ToClient.writeUTF("Divide by Zero Exception");
+                First = Long.parseLong(st.nextToken());
+                Second = Long.parseLong(st.nextToken());
+                if (Second == 0) {
+                    ToClient.writeUTF("Incorrect : Divide by Zero Exception");
+                }
                 else
-                    ToClient.writeUTF(Integer.toString(First / Second));
+                    ToClient.writeUTF(Long.toString(First / Second));
             }
-            default -> ToClient.writeUTF("Command Format Exception : Enter only Uppercase");
+            case "EXIT" -> {
+                ToClient.writeUTF("Program is now terminating....");
+                System.exit(0);
+            }
+            default -> ToClient.writeUTF("Incorrect : wrong command");
         }
     }
 }
