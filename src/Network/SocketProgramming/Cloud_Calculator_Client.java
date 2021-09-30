55cba5be5813945cf4868ae9aa40b986bfd51424
Owen-Choi <oldstyle482@gmail.com>
2021-09-30 20:35:28 +0900

    네트워크 수업 : Cloud calculator (Complete)

--- src/Network/SocketProgramming/Cloud_Calculator_Client.java
+++ src/Network/SocketProgramming/Cloud_Calculator_Client.java
@@ -10,28 +10,31 @@ public class Cloud_Calculator_Client {
     static int Pnum;
     public static void main(String[] args) throws Exception{
         try {
+            // 파일로부터 IP주소와 포트 번호를 읽어옵니다. ::
             File file = new File("C:\\Users\\oldst\\Desktop\\201835539 최철웅\\server_info.dat");
             byte[] binary = new byte[(int)file.length()];
+            // file에서 값을 읽어오기 위해 InputStream을 선언합니다. ::
             InputStream is = new FileInputStream(file);
             is.read(binary);
             is.close();
+            // tokenize return value ::
             st = new StringTokenizer(new String(binary));
             IPAddress = st.nextToken();
             Pnum = Integer.parseInt(st.nextToken());
         }catch(Exception e) {
             System.out.println(e.toString());
+            IPAddress = "127.0.0.1";
+            Pnum = 10002;
         }
-        // pre-defined "myself" IP address ::
-        // 이제 이 변수 2개를 파일에서 읽어오는 작업 필요함 ::
-        // connect to server by using IPAddress and Pnum ::
+        // 파일로부터 읽어온 IP주소와 포트번호를 이용해서 서버에 연결합니다. ::
         Socket client = new Socket(IPAddress, Pnum);
-        // Input stream to get input from user ::
+        // 유저로부터 입력을 받기 위해 BufferedReader를 선언합니다. ::
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // Input stream to get input from server ::
+        // Server로부터 입력을 받기 위해 DataInputStream을 선언합니다. ::
         DataInputStream FromServer = new DataInputStream(client.getInputStream());
-        // Output stream to give output to server ::
+        // 서버에게 출력을 주기 위해 DataOutputStream을 선언합니다. ::
         DataOutputStream ToServer = new DataOutputStream(client.getOutputStream());
-        // brief guide about cloud calculator's protocol : command ::
+        // 계산기 프토로콜을 간단하게 안내합니다. ::
         System.out.println(":: Welcome to Cloud Calculator! ::");
         System.out.println("Please Enter Command with the format : ");
         System.out.println("\t (Arithmetic Expression) Integer1 Integer2");
@@ -41,11 +44,16 @@ public class Cloud_Calculator_Client {
         while(true) {
             System.out.print("Please Enter the Command : ");
                 try {
+                    //유저로부터 커맨드를 입력받습니다. ::
                     Command = br.readLine();
+                    // 서버에게 커맨드를 보냅니다. ::
                     ToServer.writeUTF(Command);
+                    // 서버로부터 받은 문자열을 출력합니다. (답이든 오류든) ::
                     System.out.println(FromServer.readUTF());
+                    // 만약 유저의 커맨드의 시작부분이 EXIT로 시작한다면 클라이언트 프로그램 종료. ::
                     st = new StringTokenizer(Command," ");
                     if(st.nextToken().toUpperCase().equals("EXIT")) {
+                        client.close();
                         System.exit(0);
                     }
                 } catch (IOException e) {
--- src/Network/SocketProgramming/Cloud_Calculator_Server.java
+++ src/Network/SocketProgramming/Cloud_Calculator_Server.java
@@ -6,47 +6,52 @@ import java.net.Socket;
 import java.util.StringTokenizer;
 
 public class Cloud_Calculator_Server {
-    static StringTokenizer st;                              //For indentify command came from client
+    static StringTokenizer st;
     public static void main(String[] args) throws Exception{
         int Pnum = 10002;
         ServerSocket listener = new ServerSocket(Pnum);
         System.out.println("Waiting for client : ");
-        // binding client socket ::
+        // 클라이언트 소켓을 accept 합니다. ::
         Socket ConnectedSocket = listener.accept();
         System.out.println("Client connected : " + ConnectedSocket);
-        // Input stream to get input from client ::
+        // 클라이언트로부터 입력을 받기 위해 DataInputStream을 선언합니다. ::
         DataInputStream FromClient = new DataInputStream(ConnectedSocket.getInputStream());
-        // Output stream to give output to client ::
+        // 클라이언트에게 출력을 주기 위해 DataOutputStream을 선언합니다. ::
         DataOutputStream ToClient = new DataOutputStream(ConnectedSocket.getOutputStream());
         while(true) {
             try {
-                // Read command from client ::
+                // 클라이언트로부터 명령어를 읽어옵니다 ::
                 String temp = FromClient.readUTF();
-                // return result or error ::
-                InputProcessor(temp, ToClient);
-                // if there isn't client, exception may occur ::
+                // 계산함수를 호출하여 결과 혹은 오류를 클라이언트에게 전달합니다 ::
+                InputProcessor(temp, ToClient, listener, ConnectedSocket);
+                // 클라이언트가 없다면, 서버를 종료합니다. ::
             } catch (IOException e) {
                 System.out.println("There's no more client here. terminating program");
                 System.exit(0);
             }
         }
     }
-    static void InputProcessor(String Input, DataOutputStream ToClient) throws IOException{
+    static void InputProcessor(String Input, DataOutputStream ToClient, ServerSocket serverSocket, Socket client) throws IOException{
         st = new StringTokenizer(Input, " ");
         String CommandHeader = st.nextToken();
+        // 만약 명령어의 시작부분이 EXIT 라면 프로그램을 종료합니다. ::
         if(CommandHeader.toUpperCase().equals("EXIT")) {
             ToClient.writeUTF("Program is now terminating process....");
+            try {
+                Close(serverSocket, client);
+            }catch(Exception e) {
+                System.out.println(e.toString());
+            }
             System.exit(0);
         }
-        // If the token number is less than 3, then the command Parameter is out of format ::
+        // 만약 토큰의 개수가 2개가 아니라면, 많고 적음을 따져서 오류를 발생시킵니다. ::
         if(st.countTokens() != 2) {
             ToClient.writeUTF(st.countTokens() > 2 ? "Incorrect : Too many arguments" : "Incorrect : argument is not enough");
             return;
         }
         long First, Second;
         long result;
-        // We provide case ignorance ::
-        // if the first token of command is EXIT, then terminate the program ::
+        // 계산기는 대소문자에 관계없이 동작합니다. ::
         switch (CommandHeader.toUpperCase()) {
             case "ADD" -> {
                 try {
@@ -54,7 +59,7 @@ public class Cloud_Calculator_Server {
                     Second = Long.parseLong(st.nextToken());
                     result = First + Second;
                     ToClient.writeUTF(Long.toString(result));
-                    // if Input values exceed integer range, then exception(number format) will be occured ::
+                    // 만약 입력받은 First나 Second 값이 숫자가 아니거나 범위를 넘어서게 되면 오류를 발생시킵니다. ::
                 }catch(Exception e) {
                     ToClient.writeUTF(e.toString());
                 }
@@ -83,15 +88,19 @@ public class Cloud_Calculator_Server {
             case "DIV" -> {
                 First = Long.parseLong(st.nextToken());
                 Second = Long.parseLong(st.nextToken());
-                // if the second value is Zero, then divide by zero exception will be occur ::
+                // 만약 두번째 입력값이 0이라면, 오류를 발생시킵니다. ::
                 if (Second == 0) {
                     ToClient.writeUTF("Incorrect : Divide by Zero Exception");
                 }
                 else
                     ToClient.writeUTF(Long.toString(First / Second));
             }
-            // if the command format is ignored, then command violation exception will be occur ::
+            // 명령어 프로토콜이 지켜지지 않았다면 오류를 발생시킵니다. ::
             default -> ToClient.writeUTF("Incorrect : wrong command");
         }
+    }
+    static void Close(ServerSocket serverSocket, Socket client) throws Exception{
+        serverSocket.close();
+        client.close();
     }
 }
