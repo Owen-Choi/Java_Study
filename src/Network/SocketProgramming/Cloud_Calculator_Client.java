5e4711c4a3700411de00bbb78eb86f623b30cb70
Owen-Choi <oldstyle482@gmail.com>
2021-09-30 17:13:10 +0900

    네트워크 수업 : Cloud calculator

--- src/Network/SocketProgramming/Cloud_Calculator_Client.java
+++ src/Network/SocketProgramming/Cloud_Calculator_Client.java
@@ -2,26 +2,52 @@ package Network.SocketProgramming;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 public class Cloud_Calculator_Client {
+    static StringTokenizer st;
+    static String IPAddress;
+    static int Pnum;
     public static void main(String[] args) throws Exception{
-        String IPAddress = "127.0.0.1";
-        int Pnum = 10002;
+        try {
+            File file = new File("C:\\Users\\oldst\\Desktop\\201835539 최철웅\\server_info.dat");
+            byte[] binary = new byte[(int)file.length()];
+            InputStream is = new FileInputStream(file);
+            is.read(binary);
+            is.close();
+            st = new StringTokenizer(new String(binary));
+            IPAddress = st.nextToken();
+            Pnum = Integer.parseInt(st.nextToken());
+        }catch(Exception e) {
+            System.out.println(e.toString());
+        }
+        // pre-defined "myself" IP address ::
         // 이제 이 변수 2개를 파일에서 읽어오는 작업 필요함 ::
+        // connect to server by using IPAddress and Pnum ::
         Socket client = new Socket(IPAddress, Pnum);
+        // Input stream to get input from user ::
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // Input stream to get input from server ::
         DataInputStream FromServer = new DataInputStream(client.getInputStream());
+        // Output stream to give output to server ::
         DataOutputStream ToServer = new DataOutputStream(client.getOutputStream());
+        // brief guide about cloud calculator's protocol : command ::
         System.out.println(":: Welcome to Cloud Calculator! ::");
         System.out.println("Please Enter Command with the format : ");
         System.out.println("\t (Arithmetic Expression) Integer1 Integer2");
         System.out.println("\t Possible Arithmetic Expression : ADD, MINUS, DIV, MULTIPLE \n");
         System.out.println("\t If you want to terminate calculator, please enter 'Exit' ");
+        String Command;
         while(true) {
             System.out.print("Please Enter the Command : ");
                 try {
-                    ToServer.writeUTF(br.readLine());
+                    Command = br.readLine();
+                    ToServer.writeUTF(Command);
                     System.out.println(FromServer.readUTF());
+                    st = new StringTokenizer(Command," ");
+                    if(st.nextToken().toUpperCase().equals("EXIT")) {
+                        System.exit(0);
+                    }
                 } catch (IOException e) {
                     System.out.println("Error from Client");
                 }
--- src/Network/SocketProgramming/Cloud_Calculator_Server.java
+++ src/Network/SocketProgramming/Cloud_Calculator_Server.java
@@ -6,19 +6,25 @@ import java.net.Socket;
 import java.util.StringTokenizer;
 
 public class Cloud_Calculator_Server {
-    static StringTokenizer st;
+    static StringTokenizer st;                              //For indentify command came from client
     public static void main(String[] args) throws Exception{
         int Pnum = 10002;
         ServerSocket listener = new ServerSocket(Pnum);
         System.out.println("Waiting for client : ");
+        // binding client socket ::
         Socket ConnectedSocket = listener.accept();
         System.out.println("Client connected : " + ConnectedSocket);
+        // Input stream to get input from client ::
         DataInputStream FromClient = new DataInputStream(ConnectedSocket.getInputStream());
+        // Output stream to give output to client ::
         DataOutputStream ToClient = new DataOutputStream(ConnectedSocket.getOutputStream());
         while(true) {
             try {
+                // Read command from client ::
                 String temp = FromClient.readUTF();
+                // return result or error ::
                 InputProcessor(temp, ToClient);
+                // if there isn't client, exception may occur ::
             } catch (IOException e) {
                 System.out.println("There's no more client here. terminating program");
                 System.exit(0);
@@ -27,20 +33,28 @@ public class Cloud_Calculator_Server {
     }
     static void InputProcessor(String Input, DataOutputStream ToClient) throws IOException{
         st = new StringTokenizer(Input, " ");
+        String CommandHeader = st.nextToken();
+        if(CommandHeader.toUpperCase().equals("EXIT")) {
+            ToClient.writeUTF("Program is now terminating process....");
+            System.exit(0);
+        }
         // If the token number is less than 3, then the command Parameter is out of format ::
-        if(st.countTokens() != 3) {
-            ToClient.writeUTF(st.countTokens() > 3 ? "Incorrect : Too many arguments" : "Incorrect : argument is not enough");
+        if(st.countTokens() != 2) {
+            ToClient.writeUTF(st.countTokens() > 2 ? "Incorrect : Too many arguments" : "Incorrect : argument is not enough");
             return;
         }
         long First, Second;
         long result;
-        switch (st.nextToken().toUpperCase()) {
+        // We provide case ignorance ::
+        // if the first token of command is EXIT, then terminate the program ::
+        switch (CommandHeader.toUpperCase()) {
             case "ADD" -> {
                 try {
                     First = Long.parseLong(st.nextToken());
                     Second = Long.parseLong(st.nextToken());
                     result = First + Second;
                     ToClient.writeUTF(Long.toString(result));
+                    // if Input values exceed integer range, then exception(number format) will be occured ::
                 }catch(Exception e) {
                     ToClient.writeUTF(e.toString());
                 }
@@ -69,16 +83,14 @@ public class Cloud_Calculator_Server {
             case "DIV" -> {
                 First = Long.parseLong(st.nextToken());
                 Second = Long.parseLong(st.nextToken());
+                // if the second value is Zero, then divide by zero exception will be occur ::
                 if (Second == 0) {
                     ToClient.writeUTF("Incorrect : Divide by Zero Exception");
                 }
                 else
                     ToClient.writeUTF(Long.toString(First / Second));
             }
-            case "EXIT" -> {
-                ToClient.writeUTF("Program is now terminating....");
-                System.exit(0);
-            }
+            // if the command format is ignored, then command violation exception will be occur ::
             default -> ToClient.writeUTF("Incorrect : wrong command");
         }
     }
